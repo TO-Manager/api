9760d91cc1efb8cd275cb706e586695498025e39
MENT3 <clement.rms@gmail.com>
2020-03-06 23:20:09 +0100

    :construction: Setup JWT

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,6 +7,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * ApiResource()
  */
 class User implements UserInterface
 {
