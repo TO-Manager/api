62e18f0856e75dabbd354dedbd2db12f23591d47
MENT3 <clement.rms@gmail.com>
2020-03-07 15:57:53 +0100

    :sparkles: Feat: Add Adminer.php

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\User;
+use App\Entity\Credentials;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -31,6 +32,23 @@ class AppFixtures extends Fixture
                 ->setRoles(array("ROLE_USER"));
 
         $manager->persist($Sheraw);
+
+        
+        $MENT3_PASSWORD = new Credentials();
+        $MENT3_PASSWORD ->setUrl("facebook")
+                        ->setLogin('login')
+                        ->setPassword($this->encoder->encodePassword($MENT3, 'password'))
+                        ->setUser($MENT3);
+
+        $manager->persist($MENT3_PASSWORD);
+
+        $Sheraw_PASSWORD = new Credentials();
+        $Sheraw_PASSWORD ->setUrl("facebook")
+                        ->setLogin('login')
+                        ->setPassword($this->encoder->encodePassword($Sheraw, 'password'))
+                        ->setUser($Sheraw);
+                        
+        $manager->persist($Sheraw_PASSWORD);
 
         $manager->flush();
     }
